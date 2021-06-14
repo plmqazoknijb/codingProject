d092b1e4b995a09055786d8493073c48aa6c29fc
Song <w2033@e-mirim.hs.kr>
2021-06-14 10:16:58 +0900

    01 음양더하기 2단계

--- 프로그래머스/01 음양더하기.py
+++ 프로그래머스/01 음양더하기.py
@@ -1,23 +1,14 @@
 def solution(absolutes, signs):
     answer = 0
-    if signs[0] == True:
-        answer += absolutes[0]
-    else:
-        answer -= absolutes[0]
-
-    if signs[1] == True:
-        answer += absolutes[1]
-    else:
-        answer -= absolutes[1]
-
-    if signs[2] == True:
-        answer += absolutes[2]
-    else:
-        answer -= absolutes[2]
+    for i in range(3):
+        if signs[i] == True:
+            answer += absolutes[i]
+        else:
+            answer -= absolutes[i]
     return answer
 
 
 if __name__ == "__main__":
-    abs = {4, 7, 12}
+    abs = [4, 7, 12]
     s = [True, False, True]
-    print(solution(abs,s))
+    print(solution(abs, s))
