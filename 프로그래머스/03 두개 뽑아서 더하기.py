34d977a1d2f0ee073389f52ee1876d46c1fc872d
Song <w2033@e-mirim.hs.kr>
2021-06-21 10:10:47 +0900

    03 두 개 뽑아서 더하기 (set사용)

--- 프로그래머스/03 두개 뽑아서 더하기.py
+++ 프로그래머스/03 두개 뽑아서 더하기.py
@@ -1,25 +1,29 @@
-#정수 배열 numbers가 주어집니다.
-# numbers에서 서로 다른 인덱스에 있는 두 개의 수를 뽑아 더해서 만들 수 있는 모든 수를
-# 배열에 오름차순으로 담아 return 하도록 solution 함수를 완성해주세요.
+# 정수 배열 numbers가 주어집니다.
+# numbers에서 서로 다른 인덱스에 있는
+# 두 개의 수를 뽑아 더해서 만들 수 있는 모든 수를
+# 배열에 오름차순으로 담아 return 하도록
+# solution 함수를 완성해주세요.
+
 
 def solution(numbers):
-    answer =list()
-    # number[i] 는 첫 번째로 뽑은 숫자
-    # number[j] 는 두 번째로 뽑은 숫자
-    for i in range(len(numbers)):
-        for j in range(i+1,len(numbers)):
-            두수의합 = numbers[i]+numbers[j]
-            #두 수를 뽑아 더한 값이 기존에 answer에 존재하지 않을 때
-            if (두수의합) not in answer:
-                #list에 추가
-                answer.append(두수의합)
-    #오름차순으로 정렬
+    answer = list()
+    # numbers[i]는 첫 번째로 뽑은 숫자
+    # numbers[j]는 두 번째로 뽑은 숫자
+    for i in range(len(numbers)-1):
+        for j in range(i+1, len(numbers)):
+            두수의합 = numbers[i] + numbers[j]
+            answer.append(두수의합)
+    # 중복제거
+    answer = list(set(answer))
+    # 오름차순 정렬
     answer.sort()
     return answer
 
-def main():
-    numbers = [5,0,2,7]
-    print(solution(numbers))
 
-if __name__=="__main__":
+def main():
+    numbers = [5, 0, 2, 7]
+    print( solution(numbers) )
+
+
+if __name__ == "__main__":
     main()
